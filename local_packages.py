5aac5d69fb50dc9072c388816f0bfd190eb1202e
Yang Jiao <110237750+OctoberFox11@users.noreply.github.com>
2024-08-21 11:08:39 +0800

    Delete local_packages.py

--- local_packages.py
+++
@@ -1,7 +0,0 @@
-from Packages.LLM_Parser import LLMParser
-from Packages.LLM_API import LLM, MultiLLM
-from Packages.Formatter import TextDivider
-from Packages.Memory import Memory
-from Packages.LLM_BenchMarker import *
-from Packages.Multi_Process import *
-from Packages.PDF_Processor import *
